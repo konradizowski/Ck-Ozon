949bcbc85cad5c82af3c636207ac8bfd35ce4506
konradizowski <izowski@gmail.com>
2018-05-08 18:01:26 +0200

    background picture

--- js/app.js
+++ js/app.js
@@ -8,14 +8,36 @@ $(function()	{
 
 
 
+   /* const images= ['./img/adult-19033_640.jpg','./img/kitchen-2165756_640.jpg','./img/13712347603_fe51b02579_b.jpg'];
+    var nextimage=0;
 
-    function timer () {
+    doSlideshow();
+
+    function doSlideshow()
+    {
+        if($('.site-hero').length!=0)
+        {
+            $('.site-hero').fadeOut(5000,function(){slideshowFadeIn();$(this).remove()});
+        }
+        else
+        {
+            slideshowFadeIn();
+        }
+    }
+    function slideshowFadeIn()
+    {
+        $('.site-hero').prepend($('<img class="slideshowimage" src="'+images[nextimage++]+'" style="display:none">').fadeIn(500,function(){setTimeout(doSlideshow,1000);}));
+        if(nextimage>=images.length)
+            nextimage=0;
+    }*/
+
+   /* function timer () {
         setInterval(function(){
 
-                alert("Hello"); },
+                $('.site-hero').css('background-image', "url('./img/adult-19033_640.jpg')") },
 
 
             3000);
     }
-
+    timer()*/
 });
