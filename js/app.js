8213a28eef96fa7bf5a41dfde717cfa546ca3e92
konradizowski <izowski@gmail.com>
2018-05-09 15:35:41 +0200

    animations 2/3

--- js/app.js
+++ js/app.js
@@ -5,14 +5,67 @@ $(function()	{
 
     });
     $(".submenu").on('mouseleave', function() {
-        $(".submenu").slideUp('show');
+        $(".submenu").slideUp('slow');
 
     });
 
 
+    $(".box1 a ").on('mouseover', function() {
+        $(this).siblings().slideUp('slow');
+    });
+
+    $(".box1 a ").on('mouseout', function() {
+        $(this).siblings().slideDown('slow');
+    })
+
+
+    $(".box2 a ").on('mouseover', function() {
+        $(this).siblings().slideUp('slow');
+    });
+
+    $(".box2 a ").on('mouseout', function() {
+        $(this).siblings().slideDown('slow');
+    })
+
+
+    $(".box3 a ").on('mouseover', function() {
+        $(this).siblings().slideUp('slow');
+    });
+
+    $(".box3 a ").on('mouseout', function() {
+        $(this).siblings().slideDown('slow');
+    })
 
 
 
+    $(".box4 a ").on('mouseover', function() {
+        $(this).siblings().slideUp('slow');
+    });
+
+    $(".box4 a ").on('mouseout', function() {
+        $(this).siblings().slideDown('slow');
+    })
+
+
+
+
+
+
+
+
+    /*  for (var i = 0; i < imgProduct.length; i++) {
+          imgProduct[i].addEventListener("mouseover", function(){
+              var label = this.nextElementSibling;
+              label.style.display ="none";
+          });
+          imgProduct[i].addEventListener("mouseout", function(){
+
+              var label = this.nextElementSibling;
+              label.style.display ="block";
+          })
+      }*/
+
+/*
 
     const box = $(".box");
     console.log(box);
@@ -21,10 +74,11 @@ $(function()	{
        .on('mouseover', function () {
 
            console.log('hai');
-          /* var label = this.parent.lastElementChild;
-           label.style.display = "none";*/
+          /!* var label = this.parent.lastElementChild;
+           label.style.display = "none";*!/
        });
    }
+*/
 
 
    /* const images= ['./img/adult-19033_640.jpg','./img/kitchen-2165756_640.jpg','./img/13712347603_fe51b02579_b.jpg'];
